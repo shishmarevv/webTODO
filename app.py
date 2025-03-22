860a6afd3cbf54955feba070548d77bb9a4f09f5
shishmarevv <victor.shishmarev@gmail.com>
2025-03-22 20:31:31 +0100

    Docker and PostgreSQL update

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-todos = []
+# Читаем URI подключения к базе из переменной окружения, если она не задана, используем значение по умолчанию
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'postgresql://postgres:password@localhost:5432/tododb')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Модель для задания
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', todos = enumerate(todos))
+    todos = Todo.query.all()
+    return render_template('index.html', todos=todos)
 
-@app.route('/add', methods = ['POST'])
+@app.route('/add', methods=['POST'])
 def add_todo():
-    todo = request.form.get('todo')
-    if todo:
-        todos.append(todo)
+    task = request.form.get('todo')
+    if task:
+        new_todo = Todo(task=task)
+        db.session.add(new_todo)
+        db.session.commit()
     return redirect(url_for('index'))
 
-@app.route('/delete/<int:todo_id>', methods = ['GET'])
+@app.route('/delete/<int:todo_id>', methods=['GET'])
 def delete_todo(todo_id):
-    if 0 <= todo_id < len(todos):
-        todos.pop(todo_id)
+    todo = Todo.query.get(todo_id)
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    with app.app_context():  # Создаём контекст приложения
+        db.create_all()
+    app.run(debug=True, host='0.0.0.0')
